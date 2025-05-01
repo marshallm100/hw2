de09b4365637eea4a0624fe50f744f888a3f5882
Marshall Masterson <marshall.masterson@kellogg.northwestern.edu>
2025-05-01 22:07:03 +0000

    start in kmdb.rb next time

--- kmdb.rb
+++ kmdb.rb
@@ -25,6 +25,8 @@
 # - Generate the models and migration files to match the domain model from hw1.
 #   Table and columns should match the domain model. Execute the migration
 #   files to create the tables in the database. (5 points)
+
+#START HERE
 # - Insert the "Batman" sample data using ruby code. Do not use hard-coded ids.
 #   Delete any existing data beforehand so that each run of this script does not
 #   create duplicate data. (5 points)
